27d5d8144a76dee09bde5b67a41a8c7da842f642
b.nou <nou.benita@gmail.com>
2023-12-21 18:03:57 -0500

    completed 3 katas

--- src/main/java/com/nitakeepskatas/Kata.java
+++ src/main/java/com/nitakeepskatas/Kata.java
@@ -1,5 +1,8 @@
 package com.nitakeepskatas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Kata {
 
     /*                                  12-20-23
@@ -15,7 +18,7 @@ public class Kata {
         boolean result = true;
         for (int i = 0; i < alphaNum.length(); i++) {
             char leftChar = alphaNum.charAt(i);
-            char rightChar = alphaNum.charAt(alphaNum.length()-1-i);
+            char rightChar = alphaNum.charAt(alphaNum.length() - 1 - i);
             if (leftChar != rightChar) {
                 result = false;
             }
@@ -23,6 +26,52 @@ public class Kata {
         return result;
     }
 
+    /*                                  12-20-23
+        Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+        A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+        of the characters without disturbing the relative positions of the remaining characters.
+        (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+     */
+    public boolean isSubsequence(String s, String t) {
+        boolean result = false;
+
+        int sCounter = 0;
+        int tCounter = 0;
+
+        while (sCounter < s.length() && tCounter < t.length()) {
+            if (s.charAt(sCounter) == t.charAt(tCounter)) {
+                sCounter++;
+                tCounter++;
+            } else {
+                tCounter++;
+            }
+        }
+
+        if (sCounter == s.length()) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    /*                                  12-21-23
+        Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+        The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+        Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+        Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+        The remaining elements of nums are not important as well as the size of nums.
+        Return k.
+     */
+    public int removeElement(int[] nums, int val) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter;
+    }
 
 
 }
