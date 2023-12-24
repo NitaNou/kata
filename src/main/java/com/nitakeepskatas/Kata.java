9c6d7a345255fd755e66d596ac7b058aee7ff401
b.nou <nou.benita@gmail.com>
2023-12-24 00:27:38 -0500

    completed 6 katas

--- src/main/java/com/nitakeepskatas/Kata.java
+++ src/main/java/com/nitakeepskatas/Kata.java
@@ -117,4 +117,29 @@ public class Kata {
         return majority;
     }
 
+    /* (6)                                  12-24-23
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one stock and
+        choosing a different day in the future to sell that stock.
+        Return the maximum profit you can achieve from this transaction.
+        If you cannot achieve any profit, return 0.
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+     */
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            if (prices[i] - minPrice > profit) {
+                profit = prices[i] - minPrice;
+            }
+        }
+        return profit;
+    }
+
 }
