c0b92e0d0ea24dc9f004257de55ea3a88d47496b
b.nou <nou.benita@gmail.com>
2024-01-10 18:02:05 -0500

    completed 12 katas

--- src/main/java/com/nitakeepskatas/Kata.java
+++ src/main/java/com/nitakeepskatas/Kata.java
@@ -3,6 +3,7 @@ package com.nitakeepskatas;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Kata {
     /* (1)                                 12-20-23
@@ -259,8 +260,32 @@ public class Kata {
             if (!newMap.containsKey(ransomNote.charAt(i)) || newMap.get(ransomNote.charAt(i)) <= 0) {
                 return false;
             }
-            newMap.put(ransomNote.charAt(i), newMap.get(ransomNote.charAt(i))-1);
+            newMap.put(ransomNote.charAt(i), newMap.get(ransomNote.charAt(i)) - 1);
         }
+
         return true;
     }
+
+    /*  (12)                                    01-10-24
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+        Example 1:
+            Input: nums = [2,7,11,15], target = 9
+            Output: [0,1]
+            Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> complementMap = new HashMap<>(); // holds: complementValue if not the indexValue, index
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; // complement = 9 - 2 --> 7
+            if (complementMap.containsKey(complement)) {
+                return new int[]{i, complementMap.get(complement)};
+            } else {
+                complementMap.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
 }
