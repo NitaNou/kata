01cb559225cf9337eda361a0df132c6c1a31b5c6
b.nou <nou.benita@gmail.com>
2024-01-20 20:24:25 -0500

    completed 18 katas

--- src/main/java/com/nitakeepskatas/Kata.java
+++ src/main/java/com/nitakeepskatas/Kata.java
@@ -423,30 +423,31 @@ public class Kata {
     public int romanConversion(char letter) {
         int result = 0;
         switch (letter) {
-            case 'I' :
+            case 'I':
                 result = 1;
                 break;
-            case 'V' :
+            case 'V':
                 result = 5;
                 break;
-            case 'X' :
+            case 'X':
                 result = 10;
                 break;
-            case 'L' :
+            case 'L':
                 result = 50;
                 break;
-            case 'C' :
+            case 'C':
                 result = 100;
                 break;
-            case 'D' :
+            case 'D':
                 result = 500;
                 break;
-            case 'M' :
+            case 'M':
                 result = 1000;
                 break;
         }
         return result;
     }
+
     public int romanToInt(String s) {
         int currentCharValue = romanConversion(s.charAt(0));
         int sum = 0;
@@ -463,8 +464,43 @@ public class Kata {
         return sum;
     }
 
-
-
+    /*  (18)                                    01-20-24
+        Write an algorithm to determine if a number n is happy.
+        A happy number is a number defined by the following process:
+        Starting with any positive integer, replace the number by the sum of the squares of its digits.
+        Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+        Those numbers for which this process ends in 1 are happy.
+        Return true if n is a happy number, and false if not.
+        Example 1:
+            Input: n = 19
+            Output: true
+            Explanation:
+            12 + 92 = 82
+            82 + 22 = 68
+            62 + 82 = 100
+            12 + 02 + 02 = 1
+        Example 2:
+            Input: n = 2
+            Output: false
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> collectionOfSums = new HashSet<>();
+        while (n != 1) {
+            int sum = 0;
+            int currentN = n;
+            while (currentN != 0) {
+                int remainder = currentN % 10;
+                sum = sum + (remainder * remainder);
+                currentN = currentN / 10;
+            }
+            if (collectionOfSums.contains(sum)) {
+                return false;
+            }
+            n = sum;
+            collectionOfSums.add(sum);
+        }
+        return true;
+    }
 
 
 }
