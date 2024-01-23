1926ca86fbec83669ab462075725694f93a7fea9
b.nou <nou.benita@gmail.com>
2024-01-22 21:48:43 -0500

    completed 21 katas

--- src/main/java/com/nitakeepskatas/Main.java
+++ src/main/java/com/nitakeepskatas/Main.java
@@ -1,5 +1,7 @@
 package com.nitakeepskatas;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Kata kata = new Kata();
@@ -7,12 +9,18 @@ public class Main {
 //        int romanTest = kata.romanToInt("MCMXCIV");
 //        System.out.println(romanTest);
 
-        boolean test1 = kata.wordPattern("abba", "dog cat cat dog");
-        boolean test2 = kata.wordPattern("abba", "dog cat cat fish");
-        boolean test3 = kata.wordPattern("aaaa", "dog cat cat dog");
-        System.out.println(test1);
-        System.out.println(test2);
-        System.out.println(test3);
+//        boolean test1 = kata.wordPattern("abba", "dog cat cat dog");
+//        boolean test2 = kata.wordPattern("abba", "dog cat cat fish");
+//        boolean test3 = kata.wordPattern("aaaa", "dog cat cat dog");
+//        System.out.println(test1);
+//        System.out.println(test2);
+//        System.out.println(test3);
 
+        String s = "rat";
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        System.out.println(Arrays.toString(count));
     }
 }
